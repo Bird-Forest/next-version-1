67cbb33d55a037895e68e228d94206e424abe1aa
Iryna_Ptashka <bird.forest70@gmail.com>
2024-01-16 20:45:28 +0200

    It is not finish

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 // import { Metadata } from "next";
 import React, { useEffect, useState } from "react";
+import { revalidatePath } from "next/cache";
 // import styles from "../page.module.css";
 import { getAllPosts } from "@/services/getPosts";
 import PostSearch from "@/components/PostSearch";
 import Posts from "@/components/Posts";
+import Link from "next/link";
+import NewPostForm from "@/components/NewPostForm";
 
 // export const metadata: Metadata = {
 //   title: " Blog | Next App",
 // };
+
+export const revalidate = 10;
 
 export default function Blog() {
   const [posts, setPosts] = useState<any[]>([]);
@@ -25,6 +30,7 @@ export default function Blog() {
   return (
     <>
       <h1>Blog page</h1>
+      <Link href="/blog/new">Add new post</Link>
       <PostSearch onSearch={setPosts} />
       {loading ? <h3>Loading ...</h3> : <Posts posts={posts} />}
     </>
